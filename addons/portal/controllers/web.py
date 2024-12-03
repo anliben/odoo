1407ad331db5cf1d39bf0431b9f40db3b7c01ced
João Victor <anliben.yayoi@gmail.com>
2024-12-02 21:37:50 -0300

    mudando rota de redirect de login

--- addons/portal/controllers/web.py
+++ addons/portal/controllers/web.py
@@ -12,16 +12,16 @@ class Home(WebHome):
     @http.route()
     def index(self, *args, **kw):
         if request.session.uid and not is_user_internal(request.session.uid):
-            return request.redirect_query('/my', query=request.params)
+            return request.redirect_query('/slides/informacoes-inicias-1', query=request.params)
         return super().index(*args, **kw)
 
     def _login_redirect(self, uid, redirect=None):
         if not redirect and not is_user_internal(uid):
-            redirect = '/my'
+            redirect = '/slides/informacoes-inicias-1'
         return super()._login_redirect(uid, redirect=redirect)
 
     @http.route()
     def web_client(self, s_action=None, **kw):
         if request.session.uid and not is_user_internal(request.session.uid):
-            return request.redirect_query('/my', query=request.params)
+            return request.redirect_query('/slides/informacoes-inicias-1', query=request.params)
         return super().web_client(s_action, **kw)
